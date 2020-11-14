8b01b5da4edd76f91fba7db237835d04397389ce
JoseOgam <jotienoochieng@gmail.com>
2020-11-14 08:54:30 -0500

    react props, components, state and useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hello React</h2>
+      <Child name=" I'm a child component " />
+      <Checkbox />
     </div>
   );
 }
-
+const Child = (props) => {
+  return (
+    <div>
+      <h3>{props.name}</h3>
+     </div>
+   )
+}
+ 
+const Checkbox = () => {
+  let [checked, setChecked] = useState(false)
+  useEffect(() => {
+    alert(`you are about to change component state to ${checked}`)
+  })
+  return (
+    <div>
+      <input onChange={()=>setChecked(!checked)} type="checkbox" value={checked} />
+      {checked ? "checkbox is marked" : "checkbox is unmarked"}
+    </div>
+  )
+}
 export default App;
